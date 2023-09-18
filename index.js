2a8de7aaa15745891135293ae904622d3c337235
calebmutisya <mutisyacalebmusau@gmail.com>
2023-09-18 17:45:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+function equation(){
+    return multiply;
+}
+const random = Math.floor(Math.random()*100)+1;
+function randomNumberGreaterThanZero(){
+    return random;
+}
+const num3=24;
+const num4=5;
+const mod= num3%num4;
+function modEquation(){
+    return mod;
+}
+const max=Math.max(2,5,9,20,16);
+function maxEquation(){
+    return max;
+}
